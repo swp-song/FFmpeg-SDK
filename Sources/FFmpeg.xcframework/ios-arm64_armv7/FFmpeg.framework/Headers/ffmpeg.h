140abc524aca9ace1871dce212745487e03415b2
Dream <396587868@qq.com>
2021-07-17 11:32:45 +0800

    Release v1.1.0

--- Sources/FFmpeg.xcframework/ios-arm64_armv7/FFmpeg.framework/Headers/ffmpeg.h
+++ Sources/FFmpeg.xcframework/ios-arm64_armv7/FFmpeg.framework/Headers/ffmpeg.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47ef0b54af5de29d76b5928e6a883ef34ba186905a8bf35903463afb61d09f78
-size 369
+oid sha256:559f6565a6b623026ab71fa5871e096d464ef0ce6e9341c0b07a4427c375b6b9
+size 362
